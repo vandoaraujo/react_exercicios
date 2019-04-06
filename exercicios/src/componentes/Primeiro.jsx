f7f12e5b9156194206cfc3c7fa86a4f0871a93bc
Vando Araujo <tecladista.vando@gmail.com>
2019-04-06 12:43:45 -0300

    criacao do projeto

---
+++ exercicios/src/componentes/Primeiro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//Eu preciso importar o react, porque a funcao abaixo usa JSX e ela
+//necessitará das funções do React
+
+// export default function() {
+//     return <h1>Primeiro</h1>
+// }
+
+//Posso escrever desses dois modos, abaixo estou usando uma aerofunction
+
+export default () => <h1>Primeiro</h1>
--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Primeiro from './componentes/Primeiro'
+
 // essa é uma sintaxe do React chamda JSX que será transpilada
 //Esse elemento virará JS
 //Voce deixará de separar os arquivos por tipo e eles funcionarao como componentes
-const elemento = <h1>React</h1>
-ReactDOM.render(elemento, document.getElementById('root'))
+//*A REACTDOM conversa com a DOM
+ReactDOM.render(<Primeiro /> , document.getElementById('root'))
