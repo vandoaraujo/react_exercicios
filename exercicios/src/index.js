efe40ca7ccc6c321932c18334987c653fd7f04ed
Vando Araujo <tecladista.vando@gmail.com>
2019-04-08 19:15:54 -0300

    criacao exercicio

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// import Bomdia from './componentes/Bomdia'
 
-import Primeiro from './componentes/Primeiro'
+//usando o modulo de desestruturacao, estamos exportando a funcao BoaTarde, BoaNoite
+import {BoaTarde, BoaNoite} from './componentes/Multiplos' 
 
 // essa é uma sintaxe do React chamda JSX que será transpilada
 //Esse elemento virará JS
 //Voce deixará de separar os arquivos por tipo e eles funcionarao como componentes
 //*A REACTDOM conversa com a DOM
-ReactDOM.render(<Primeiro /> , document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <BoaTarde nome='Vando' />
+        <BoaNoite nome='Flavia' />
+    </div>
+, document.getElementById('root'))
